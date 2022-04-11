1ec4bf257469d38f872eabfd1339b83ed1570404
Gerald Ezenagu <egerald344@gmail.com>
2022-04-11 21:35:20 +0100

    working on styles

--- front-end/src/shared/components/Navigation/MainNavigation.js
+++ front-end/src/shared/components/Navigation/MainNavigation.js
@@ -1,21 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
 import MainHeader from "./MainHeader";
 import {Link} from 'react-router-dom';
 import SideDrawer from "./SideDrawer";
 
 import './MainNavigation.css'
 import NavLinks from "./NavLinks";
+import Backdrop from "../UIElements/Backdrop/Backdrop";
 
 const MainNavigation = (props) => {
+    const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+    const openDrawer = () => {
+        setDrawerIsOpen(true);
+    };
+
+    const closeDrawer = () => {
+        setDrawerIsOpen(false);
+    };
     return (
         <React.Fragment>
-        <SideDrawer>
+            {drawerIsOpen && <Backdrop onClick={closeDrawer} />}
+            {drawerIsOpen && (<SideDrawer>
             <nav className="main-navigation__drawer-nav">
                 <NavLinks />
             </nav>
-        </SideDrawer> 
+        </SideDrawer>
+        )}
+         
         <MainHeader>
-            <button className="main-navigation__menu-btn">
+            <button className="main-navigation__menu-btn" onClick={openDrawer}>
                 <span />
                 <span />
                 <span />
--- front-end/src/user/components/UserList.js
+++ front-end/src/user/components/UserList.js
@@ -14,7 +14,7 @@ const UserList = (props) => {
             </div>
          );
     }
-    return <ul>
+    return <ul className="users-list">
         {props.items.map( user=> 
             <UserItem 
                 key={user.id} 
