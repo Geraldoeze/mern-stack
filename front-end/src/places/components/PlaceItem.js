59d17516177cb3617047e14261138140ac763e95
Gerald Ezenagu <egerald344@gmail.com>
2022-05-18 21:28:11 +0100

    added google map

--- front-end/src/places/components/PlaceItem.js
+++ front-end/src/places/components/PlaceItem.js
@@ -3,6 +3,7 @@ import Card from "../../shared/components/UIElements/Card/Card";
 import Button from "../../shared/components/FormElements/Button/Button";
 import './PlaceItem.css'
 import Modal from "../../shared/components/UIElements/Modal";
+import Map from "../../shared/components/UIElements/Map";
 
 const PlaceItem = (props) => {
     const [ showMap, setShowMap ] = useState(false);
@@ -22,7 +23,7 @@ const PlaceItem = (props) => {
           footer={<Button onClick={closeMapHandler}>CLOSE</Button>}
         >
             <div className="map-container">
-                <h2>THR MAP</h2>
+                <Map center={props.coordinates} zoom={15} />
             </div>
         </Modal>  
 
--- front-end/src/shared/components/UIElements/Map.js
+++ front-end/src/shared/components/UIElements/Map.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import './Map.css';
 
-const Map = () => {
+const Map = (props) => {
+    const mapRef = useRef();
+    const { center, zoom } = props;
+
+    useEffect(() => {
+        const map = new window.google.maps.Map(mapRef.current, {
+            center: props.center,
+            zoom: props.zoom
+        });
+    
+        new window.google.maps.Marker({position: props.center, map: map})
+    }, [center, zoom]);
+    
     return (  
-        <div className={`map ${props.className}`} style={props.style}>
-            
+        <div 
+          ref={mapRef} 
+          className={`map ${props.className}`} 
+          style={props.style}>
+
         </div>
     );
 }
