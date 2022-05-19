10971f314d3fb6413b7496bb5edc33543dc14ab7
Gerald Ezenagu <egerald344@gmail.com>
2022-05-19 09:02:08 +0100

    added validations

--- front-end/src/places/pages/NewPlaces.js
+++ front-end/src/places/pages/NewPlaces.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import "./NewPlace.css";
+import { VALIDATOR_REQUIRE } from '../../shared/util/validators';
+import Input from '../../shared/components/FormElements/Input';
 
 const NewPlace = () => {
     return (  
-        <h2>NewPlace indeed</h2>
+       <form className='place-form'>
+           <Input 
+             element="input" 
+             type="text" 
+             label="Title" 
+             validators={[VALIDATOR_REQUIRE()]}
+             errorText="Please enter a valid title."
+            />
+       </form>
     );
 }
  
