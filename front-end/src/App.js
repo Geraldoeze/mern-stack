07275cc27188b466a61dc2d950030f1cc6d8a3ba
Gerald Ezenagu <egerald344@gmail.com>
2022-07-24 21:42:31 +0100

    added useAuth

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,59 +5,18 @@ import Users from './user/pages/Users';
 import Auth from './user/pages/Auth';
 
 import { AuthContext } from './shared/context/auth-context';
+import { useAuth } from './shared/hooks/auth-hook';
 
 import NewPlace from './places/pages/NewPlaces';
-import React, {useState, useCallback, useEffect} from 'react';
+import React from 'react';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 import UserPlaces from './places/pages/UserPlaces';
 import UpdatePlace from './places/pages/UpdatePlace';
 
-let logoutTimer;
 
-const App = ( ) => {
-  const [token, setToken] = useState(null);
-  const [tokenExpDate, setTokenExpDate] = useState();
-  const [userId, setUserId] = useState(false); 
+const App = () => {
 
-  const login = useCallback((uid, token, expirationDate) => {
-    setToken(token);
-    setUserId(uid);
-    const tokenExpirationDate = expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
-    setTokenExpDate(tokenExpirationDate);
-    localStorage.setItem(
-      'userData',
-       JSON.stringify({
-         userId: uid, 
-         token: token,
-         expiration: tokenExpirationDate.toISOString() }));
-  }, []);
-
-  const logout = useCallback(() => {
-    setToken(null);
-    setTokenExpDate(null);
-    setUserId(null);
-    localStorage.removeItem('userData');
-  }, []);
-
-  useEffect(() => {
-    if (token && tokenExpDate) {
-      const remainingTime = tokenExpirationDate.getTime() - new Date().getTime();
-      logoutTimer = setTimeout(logout, remainingTime);
-    } else {
-      clearTimeout(logoutTimer);
-    }
-  }, [token, logout, tokenExpDate]);
-
-  useEffect(() => {
-    const storedData = JSON.parse(localStorage.getItem('userData'));
-    if (
-      storedData &&
-      storedData.token && 
-      new Date(storedData.expiration) > new Date()) {
-      login(storedData.userId, storedData.token, new Date(storedData.expiration));
-    }
-  }, [login]);
-
+  const { token, login, logout, userId } = useAuth();
   let routes;  
   
   if (token) {
